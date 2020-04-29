50df7b24be6ad27ceca4e04a62d842f19b6b2693
Stella Beemer <stella.beemer@gmail.com>
2020-04-29 11:55:05 -0700

    she lives

---
+++ Discord/src/main/java/Main.java
@@ -0,0 +1,30 @@
+/**
+ * testing on how to build a basic discord bot
+ * to annoy all of my friends with
+ */
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import javax.security.auth.login.LoginException;
+
+public class Main extends ListenerAdapter {
+
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        String token = "NzA1MTIxNDE4MjkxMDUyNTk0.XqnF2Q.yAImFKIrCD_xMvscvWqjYx_9wak";
+        builder.setToken(token);
+        builder.addEventListener(new Main());
+        builder.buildAsync();
+    }
+
+    public void onMessageReceived(MessageReceivedEvent event) {
+        System.out.println("We received a message from " +
+                event.getAuthor().getName() + ": " +
+                event.getMessage().getContentDisplay());
+        if(event.getMessage().getContentRaw().equals("!test")) {
+            event.getChannel().sendMessage("stan loona").queue();
+        }
+    }
+}
