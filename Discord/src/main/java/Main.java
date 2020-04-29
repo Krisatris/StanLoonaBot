2dfa1559913eac3188af8bf1ec2533310e7bd27c
Stella Beemer <stella.beemer@gmail.com>
2020-04-29 13:35:38 -0700

    bot token security flaw fixed

--- Discord/src/main/java/Main.java
+++ Discord/src/main/java/Main.java
@@ -8,12 +8,21 @@ import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main extends ListenerAdapter {
 
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         String token = "";
+        try {
+            Scanner tokenFile = new Scanner(new File("C:\\Users\\stell\\Desktop\\Java\\Hell\\bot-token.txt"));
+            token = tokenFile.next();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
         builder.setToken(token);
         builder.addEventListener(new Main());
         builder.buildAsync();
