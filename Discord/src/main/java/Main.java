72a24728c45ba30c174410d1808ba332eec8d88a
Stella Beemer <stella.beemer@gmail.com>
2020-04-29 23:19:43 -0700

    bot can enter and leave vc

--- Discord/src/main/java/Main.java
+++ Discord/src/main/java/Main.java
@@ -20,20 +20,30 @@ public class Main extends ListenerAdapter {
         try {
             Scanner tokenFile = new Scanner(new File("C:\\Users\\stell\\Desktop\\Java\\Hell\\bot-token.txt"));
             token = tokenFile.next();
+            builder.setToken(token);
+            builder.addEventListener(new Main());
+            builder.buildAsync();
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
-        builder.setToken(token);
-        builder.addEventListener(new Main());
-        builder.buildAsync();
     }
 
     public void onMessageReceived(MessageReceivedEvent event) {
+        MusicPrint musicPrint = new MusicPrint(); // dont like this because its creating a new object every time someone messages chat
         System.out.println("We received a message from " +
                 event.getAuthor().getName() + ": " +
                 event.getMessage().getContentDisplay());
-        if(event.getMessage().getContentRaw().equals("!test")) {
+        String message = event.getMessage().getContentRaw();
+        if(event.getAuthor().isBot()) {
+            return;
+        } else if(message.equals("!test")) {
             event.getChannel().sendMessage("stan loona").queue();
+        } else if(message.equals("!play")) {
+            event.getChannel().sendMessage("connecting to general").queue();
+            musicPrint.startPlaying(event);
+        } else if(message.equals("!leave")) {
+            event.getChannel().sendMessage("leaving voice").queue();
+            musicPrint.leaveVoice(event);
         }
     }
 }
